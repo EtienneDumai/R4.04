afcde0279e934950d4391268df18454a6e6ac649
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-13 20:05:31 +0200

    debut de la fiche 3 en programme python

--- fiche3.py
+++ fiche3.py
@@ -2,10 +2,9 @@ import pandas as pd
 import numpy as np
 import os 
 from mlxtend.frequent_patterns import apriori, association_rules 
-os.chdir("C:\Cours\2emeAnnee\4emeSemestre\R4.04")
-dataset = pd.read_table('panier2.csv', sep=';', encoding='ASCII')
+os.chdir("C:\\Cours\\2emeAnnee\\4emeSemestre\\R4.04")
+dataset = pd.read_table('panier2.csv', sep=';', encoding='ANSI')
 tabc= pd.crosstab(dataset.trans, dataset.produit)
 tabc.replace(0,False)
 tabc.replace(1,True)
-def apriori(df, min_support=0.5, use_colnames=False, max_len=None, verbose=0, low_memory=False):
-    return
+freq_itemsets = apriori(tabc, min_support=0.3, max_len=4, use_colnames=True)
