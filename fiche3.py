5a74fe73e0b6355c6b5eaa6c8a8d85c549118321
Etienne Dumai <etienne.dumai@gmail.com>
2025-05-16 08:11:38 +0200

    panier<2.csv fait

--- fiche3.py
+++ fiche3.py
@@ -1,10 +1,38 @@
-import pandas as pd 
-import numpy as np
-import os 
-from mlxtend.frequent_patterns import apriori, association_rules 
-os.chdir("C:\\Cours\\2emeAnnee\\4emeSemestre\\R4.04")
-dataset = pd.read_table('panier2.csv', sep=';', encoding='ANSI')
-tabc= pd.crosstab(dataset.trans, dataset.produit)
-tabc.replace(0,False)
-tabc.replace(1,True)
-freq_itemsets = apriori(tabc, min_support=0.3, max_len=4, use_colnames=True)
+import pandas as pd
+from mlxtend.frequent_patterns import apriori, association_rules
+
+def load_and_filter_data(file_path, country_filter = "", separator=';', encoding='latin1'):
+    dataset = pd.read_csv(file_path, sep=separator, encoding=encoding)
+    return dataset
+
+def prepare_data(data):
+
+    tabc = pd.crosstab(data['trans'], data['produit'])
+    
+    tabc = tabc > 0
+    return tabc
+
+def analyze_apriori(dataframe, min_support=0.3, metric="confidence", min_threshold=0.8):
+
+    frequent_itemsets = apriori(dataframe, min_support=min_support, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric=metric, min_threshold=min_threshold, num_itemsets=4)
+    return frequent_itemsets, rules
+
+
+
+file_path = "panier2.csv"  
+
+
+data = load_and_filter_data(file_path)
+
+
+tableau_bool = prepare_data(data)
+
+
+frequent_itemsets, rules = analyze_apriori(tableau_bool, min_support=0.5, metric="confidence", min_threshold=0.8)
+
+print("Itemsets fréquents :")
+print(frequent_itemsets)
+
+print("\nRègles d'association :")
+print(rules)
